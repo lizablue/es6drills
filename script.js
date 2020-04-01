a8d2e2ff02db99cf7009e97a58f4df5e77f52092
ELIZABETH <EMCCREIGHT90@GMAIL.COM>
2020-03-31 20:23:10 -0500

    arrow function drills and attemps at 9 and 10

--- script.js
+++ script.js
@@ -1,5 +1,29 @@
-function favMovie(name, movie = 'The Room') {
-    console.log(`my name is ${name} and my favorite movie is ${movie}`);
-}
+let favMovie = (name, movie = 'The Room') => console.log(`my name is ${name} and my favorite movie is ${movie}`);
 
 favMovie('liz', 'mean girls');
+
+let fullName = 'Liz McCreight'
+
+let name = fullName.split(' ');
+
+let getFirstName = (fullName) => {
+    console.log(name[0]);
+};
+getFirstName();
+
+let getFirstNameConcise = fullName => console.log(name[0]);
+getFirstNameConcise();
+
+// let multiObject = (a, b) => ({
+//     exponent: a ** b,
+//     product: a * b,
+// });
+// multiObject(2, 2);
+
+function multiObject() {
+    return 2 + 3;
+}
+multiObject();
+
+let add = (a,b) => a + b;
+add(5, 7);
